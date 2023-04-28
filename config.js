57a33a08b8d0eb19aadcd5ecb7dbe9189aa6b76f
andrewch8o.cld <andriy.chumachenko@cloudinary.com>
2023-04-27 18:26:29 -0700

    Added report file name

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
  * This file defines configuration for re-usable assets
  */
 module.exports = {
-    INPUT_FILE : 'assets-to-migrate-via-urls.csv',
-    LOG_FILE   : 'migration.log.json',
+    INPUT_FILE  : 'assets-to-migrate-via-urls.csv',
+    LOG_FILE    : 'migration.log.jsonl',
+    REPORT_FILE : 'migration.report.csv',
     MAX_CONCURRENT_UPLOADS : 3
 };
