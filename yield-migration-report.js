4fbd4ea42a30e73b2441147c342f235a1ce25f4e
andrewch8o.cld <andriy.chumachenko@cloudinary.com>
2023-04-30 16:56:58 -0700

    Extended comments. Used more descriptive names.

--- yield-migration-report.js
+++ yield-migration-report.js
@@ -1,5 +1,6 @@
 /**
- * Yields migration report (CSV) from the log file
+ * Yields migration report (CSV) from the migration log file.
+ * Uses stream processing to avoid loading the entire log file into memory.
  */
 
 const fs = require('node:fs');
@@ -8,13 +9,17 @@ const {stringify} = require('csv-stringify');
 const config = require('./config');
 
 /**
- * Converts a log line (assumed to be JSONL) to a "flat" JS object
- * that represets a CSV row in the migration report
+ * Converts a log line (assumed to be JSONL) to a "flat" JS object.
+ * The produced JS object is to be serialized to CSV text and added to report file.
+ * 
+ * Ignores all log lines that are not part of the 'migration' flow. *
+ * 
+ * Customize this function to suit your needs (e.g. add more fields to the report)
  * 
  * @param {*} logLine 
- * @returns 
+ * @returns {Object} - JS object that represets a CSV row in the migration report
  */
-function log2Report(logLine) {
+function extractMigrationFlowRecord(logLine) {
     try {
         const logRec = JSON.parse(logLine);
 
@@ -23,18 +28,18 @@ function log2Report(logLine) {
         const inputRec = logRec.input;
         const status = logRec.summary.status;
         let migrationSummaryRec = {
-            Mig_Status      : status,
-            Mig_Operation   : null,
-            Mig_Error       : null,
-            Mig_CldPublicId : null,
-            Mig_CldEtag     : null,
+            Cld_Status    : status,
+            Cld_Operation : null,
+            Cld_Error     : null,
+            Cld_PublicId  : null,
+            Cld_Etag      : null,
         };
         if (status !== 'MIGRATED') {
-            migrationSummaryRec.Mig_Error = logRec.summary.err.message;
+            migrationSummaryRec.Cld_Error = logRec.summary.err.message;
         } else {
-            migrationSummaryRec.Mig_Operation = logRec.response.overwritten ? 'Overwritten' : 'Uploaded';
-            migrationSummaryRec.Mig_CldPublicId = logRec.response.public_id;
-            migrationSummaryRec.Mig_CldEtag = logRec.response.etag;
+            migrationSummaryRec.Cld_Operation = logRec.response.overwritten ? 'Overwritten' : 'Uploaded';
+            migrationSummaryRec.Cld_PublicId = logRec.response.public_id;
+            migrationSummaryRec.Cld_Etag = logRec.response.etag;
         }
         return {...inputRec, ...migrationSummaryRec};
     } catch (err) {
@@ -47,6 +52,6 @@ const csvStringifier = stringify({
 });
 
 fs.createReadStream(config.LOG_FILE)
-  .pipe(split2(log2Report))
+  .pipe(split2(extractMigrationFlowRecord))
   .pipe(csvStringifier)
   .pipe(fs.createWriteStream(config.REPORT_FILE));
