dfd702935cf19b87ae3b4c10a043afcf8d5f5d3a
andrewch8o.cld <andriy.chumachenko@cloudinary.com>
2023-04-30 17:12:01 -0700

    Expanded description. Used more descriptive names

--- cld-api-payload.js
+++ cld-api-payload.js
@@ -1,15 +1,30 @@
-function fromCsvRecord(csvRecObj) {
-    const file = csvRecObj.Url;
+/**
+ * @fileoverview This file contains the functions to create the Cloudinary API payload from a CSV record.
+ */
+
+/**
+ * Converts a CSV record from migration input file to a Cloudinary API payload.
+ * 
+ * Customize this function to suit your needs (e.g. modify upload parameters).
+ * 
+ * @param {Object} csvRec - CSV record from the migration input file
+ * @returns {Object} - parameters for Cloudinary API call
+ *  - file: the URL to obtain the asset from
+ *  - options: options for the Cloudinary Upload API call
+ */
+function fromCsvRecord(csvRec) {
+    const file = csvRec.Url;
     const options = {
-        public_id: csvRecObj.Id,
+        public_id: csvRec.Id,
         unique_filename: false,
         resource_type: 'auto',
         type: 'upload',
-        tags: csvRecObj.Tags,
+        tags: csvRec.Tags,
         context: {
-            caption: csvRecObj.Description,
+            caption: csvRec.Description,
         }
     };
+
     return { file, options };
 }
 
