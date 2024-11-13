fe515bfa8c98a6d61526b00703e75d68ae00a503
achumachenko-cloudinary <andriy.chumachenko@cloudinary.com>
2024-11-12 17:40:48 -0800

    Updated tool version

--- cld-bulk.js
+++ cld-bulk.js
@@ -57,7 +57,7 @@ function configureProgram(program) {
     program
         .name('cld-bulk')
         .description('Extensible CLI tool to efficiently translate CSV file records into Cloudinary API operations')
-        .version('2.0.1');
+        .version('2.1.0');
 }
 
 
